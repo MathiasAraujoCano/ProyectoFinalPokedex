1796cfe36f0aa51276167520a694113a35cfb3e9
mathias <mathias.araujo2310@gmail.com>
2022-12-08 21:51:46 -0300

    register done, delete pokemon done

--- server/controllers/services.js
+++ server/controllers/services.js
@@ -3,14 +3,34 @@ const router = express.Router();
 const db = require('../models/index')
 
 
-router.get('/pkmn',(req,res) => { 
+router.get('/pkmn',(req,res) => { //done get pokemones
     const Pokemon = db.Pokedex
     Pokemon.findAll()
     .then(data => {res.send(data)})
     .catch(err => {res.status(500).send({message: err.message})})
 })
 
-router.post('/pkmn',(req,res)=>{  
+router.get('/pkmn/:id',(req,res) => { //done get pokemon by id
+    let idPoke = req.params.id
+    const Pokemon = db.Pokedex
+    
+    Pokemon.findOne({
+        where:{
+            idPokemon: idPoke
+        }
+    })
+    .then((data) => {
+        if (data) {
+            res.send(data)
+        }
+        else {
+            res.status(404).send({ message: "Pokemon no disponible" })
+        }
+    })
+    .catch(err => {res.status(400).send({ message: err.message })})
+})
+
+router.post('/pkmn',(req,res)=>{  //done post pokemon
     const Pokemon = db.Pokedex
     let poke = {
         idPokemon: req.body.idPokemon,
@@ -32,7 +52,29 @@ router.post('/pkmn',(req,res)=>{
     })
 })
 
-router.get('/user',(req,res) => { //done
+
+router.delete('/pkmn/:id',(req,res) => {  //done delete pokemon by id
+    let idPoke = req.params.id
+    const Pokemon = db.Pokedex
+    
+    Pokemon.destroy({
+        where:{
+            idPokemon: idPoke
+        }
+    })
+    .then((data) => { 
+        if (data) {
+            res.send({ message: "Pokemon borrado con éxito"})
+        }
+        else {
+            res.status(404).send({ message: "Pokemon no existe en la base de datos" })
+        }
+    })
+    .catch(err => {res.status(400).send({ message: err.message })})
+})
+
+
+router.get('/user',(req,res) => { //done get users
     const People = db.User
     People.findAll()
     .then(data => {res.send(data)})
