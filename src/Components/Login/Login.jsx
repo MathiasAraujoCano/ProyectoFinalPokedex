6801de29594f57726d6ff2c5a600442712469ed8
Yayi728 <yaelcalcagno@gmail.com>
2022-12-09 21:46:38 -0300

    por fin login

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,22 +1,36 @@
 import React from "react";
+import Classes from "../Login/Login.module.css"
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 const Login = (props) => {
 
-    return(
-        <div className="Login">
-            <img className="LogoLogin" src="Materials\LogoLogin.png" alt="" />
-            <div className="MainLogin">
-                <p className="pokeball"></p>
-                <form className="Inputs">
-                    <label>
-                        <input className="InputLogin" type="email" name="Email"/>
-                        <input className="InputLogin" type="password" name="Contraseña" />
-                    </label>
-                    <br />
-                    <input type="submit" value="Submit" />
-                </form>
-            </div>
-        </div>
+        return(
+           
+           <Form >
+                <img className={Classes.LogoLogin} src="Materials\LogoLogin.png" alt="" />            
+                    
+                    <div className={Classes.FormLogin}>
+                    <Form.Group className={Classes.formEmail} controlId="formEmail">
+                        <Form.Label className={Classes.Email}>Email</Form.Label>
+                        <Form.Control type="email" placeholder="Ingresar Email" />
+                    </Form.Group>
+                    
+                    <Form.Group className={Classes.formPassword} controlId="formPassword">
+                        <Form.Label className={Classes.Contraseña}>Contraseña</Form.Label>
+                        <Form.Control type="password" placeholder="Contraseña" />
+                    </Form.Group>
+                    
+                    <Button className={Classes.LoginIniciar} type="submit">
+                        Iniciar Sesión
+                    </Button>
+                    <br>
+                    </br>
+                    <Button className={Classes.Registrarse} type="submit">
+                        Registrarse!
+                    </Button>
+                    </div>
+          </Form>
     )
 }
 
