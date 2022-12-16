3e2b6ca1f0236fbc78a0b7e6a5b2c2e7bbf3e59d
Majo <majoaraujopiedra@outlook.com>
2022-12-16 19:37:09 -0300

    Loading Page done

--- cliente/src/Components/LoadingPage/LoadingPage.jsx
+++ cliente/src/Components/LoadingPage/LoadingPage.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import Classes from "../LoadingPage/LoadingPage.module.css"
 const LoadingPage = (props) => {
     
     return(
-        <p>Cargando</p>
+        <div className={Classes.MainContainer}>
+            <img className={Classes.Img} src="\Materials\CharmanderGif.gif" alt="" />
+        </div>
     )
 }
 
